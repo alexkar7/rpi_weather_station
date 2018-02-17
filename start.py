cf7c69e8317e218356e970f3a04c34b267da3d4f
alejandro <acarmona@bytedealers.com>
2018-02-17 22:25:48 +0100

    17-02-2018 - 22h25:
    - Enabling communication to Carriots platform to upload sensors data

--- start.py
+++ start.py
@@ -2,5 +2,4 @@
 from managers import weather_station
 
 if __name__ == '__main__':
-    print(weather_station.get_humidity())
-    print(weather_station.get_temperature())
+    weather_station.upload_data_2_carriots()
